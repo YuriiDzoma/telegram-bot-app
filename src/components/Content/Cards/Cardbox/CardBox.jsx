2b1831155a627e2e5c82c67814f6d223501ec739
YuriiDzoma <devastator321678>
2023-10-02 21:36:25 +0300

    update 4.25

--- src/components/Content/Cards/Cardbox/CardBox.jsx
+++ src/components/Content/Cards/Cardbox/CardBox.jsx
@@ -1,27 +1,40 @@
-// import React from "react";
-// import './CardBox.css';
-//
-// const CardBox = ({value, number, code, date, type}) => {
-//     return (
-//         <div className={'card'}>
-//             <div className={'cardSettings'}>
-//                 <button>O</button>
-//             </div>
-//             <p className={'cardValue'}>
-//                 <span className={'cardValue__current'}>{value} </span>
-//                 /
-//                 <span className={'cardValue__limit'}> 5000</span>
-//             </p>
-//             <div className={'cardNumber'}>
-//                 <p>{number}</p>
-//                 <p>{code}</p>
-//             </div>
-//             <div className={'cardDate'}>
-//                 <p>{date}</p>
-//                 <p>{type}</p>
-//             </div>
-//         </div>
-//     )
-// }
-//
-// export default CardBox;
+import React from "react";
+
+const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+};
+
+const CardBox = ({value, number, code, date, type}) => {
+
+    const cardBox = {
+        padding: '16px',
+        backgroundColor: '#3E4449',
+        boxShadow: '0 0 13px 0 #0C0B0B26',
+        marginTop: '16px',
+    }
+
+    return (
+        <div style={cardBox}>
+            <div>
+                <button>O</button>
+            </div>
+            <p>
+                <span>{value} </span>
+                /
+                <span> 5000</span>
+            </p>
+            <div>
+                <p>{number}</p>
+                <p>{code}</p>
+            </div>
+            <div>
+                <p>{date}</p>
+                <p>{type}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CardBox;
