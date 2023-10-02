8543bb06814668ca00e01c486b2902fccb1818c0
YuriiDzoma <devastator321678>
2023-10-02 17:10:33 +0300

    update 4.17

--- src/components/Content/Cards/CardsList.jsx
+++ src/components/Content/Cards/CardsList.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import {useSelector} from "react-redux";
-
+import {Swiper, SwiperSlide} from 'swiper/react';
 
 const CardsList = () => {
 
     const allCards = useSelector(state => state.wallet.cards);
 
+
     return (
         <div>
-            {allCards.map((card, index) => <p key={index}>{card.cardValue}</p> )}
+            {/*{allCards.map((card, index) => <p key={index}>{card.cardValue}</p> )}*/}
+
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
         </div>
     )
 }
