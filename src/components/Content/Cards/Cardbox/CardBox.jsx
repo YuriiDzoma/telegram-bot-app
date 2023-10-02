dd84f12453dd23b61b859f8405b8f58559eb0cb5
YuriiDzoma <devastator321678>
2023-10-02 17:33:48 +0300

    update 4.23

--- src/components/Content/Cards/Cardbox/CardBox.jsx
+++ src/components/Content/Cards/Cardbox/CardBox.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import styles from './CardBox.module.scss'
 
 const CardBox = ({value, number, code, date, type}) => {
     return (
-        <div className={styles.card}>
-            <div className={styles.cardSettings}>
-                <button>O</button>
-            </div>
-            <div className={styles.cardNumber}>
-                <p>{number}</p>
-                <p>{code}</p>
-            </div>
-            <div className={styles.cardDate}>
-                <p>{date}</p>
-                <p>{type}</p>
-            </div>
+        <div>
+            <p>{value}</p>
+            <p>{number}</p>
+            <p>{code}</p>
+            <p>{date}</p>
+            <p>{type}</p>
         </div>
+        // <div className={styles.card}>
+        //     <div className={styles.cardSettings}>
+        //         <button>O</button>
+        //     </div>
+        //     <p className={styles.cardValue}>
+        //         <span className={styles.cardValue__current}>{value} </span>
+        //         /
+        //         <span className={styles.cardValue__limit}> 5000</span>
+        //     </p>
+        //     <div className={styles.cardNumber}>
+        //         <p>{number}</p>
+        //         <p>{code}</p>
+        //     </div>
+        //     <div className={styles.cardDate}>
+        //         <p>{date}</p>
+        //         <p>{type}</p>
+        //     </div>
+        // </div>
     )
 }
 
