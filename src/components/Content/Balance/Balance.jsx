426c55c9bd5f23efd741e853088fed9ba8f1cfee
YuriiDzoma <devastator321678>
2023-10-02 22:44:44 +0300

    update 4.8

--- src/components/Content/Balance/Balance.jsx
+++ src/components/Content/Balance/Balance.jsx
@@ -3,12 +3,19 @@ import styles from './Balance.module.scss';
 import Button from "../../Button/Button";
 
 const Balance = () => {
+    const {telegram, onToggleButton} = useTelegram();
+
+    useEffect(() => {
+        telegram.ready();
+    }, [])
+
+    const theme = telegram.colorScheme;
 
     const [balance, setBalance] = useState(293);
 
     return (
         <div className={styles.contentWrapper}>
-            <h2 className={styles.balance}>USDT {balance}</h2>
+            <h2 className={theme === 'dark' ? styles.balance : styles.balanceLight}>USDT {balance}</h2>
             <Button plus={true}>
                 <span>Replenish</span>
             </Button>
