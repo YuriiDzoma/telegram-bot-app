5434c84e2ebd99d4c147348a2ad2998dfb475dc4
YuriiDzoma <devastator321678>
2023-11-27 16:54:20 +0200

    update 7.2

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,25 @@
-const baseUrl = 'https://ce-bot.com/api/v2/home/start_web_app/'
+const baseUrl = 'https://ce-bot.com/api/v2/home/start_web_app/';
+import axios from 'axios';
 
-const getUserInfo = async ({id}) => {
-    fetch(`${baseUrl}${id}`, {
+export const getUserInfo = async () => {
+    fetch(`${baseUrl}${463697926}`, {
         method: "GET",
         headers: {
             Authorization: 'Token af9bbdda4b98828b15e3a37207eed08f02dbd33a',
         },
     })
-        .then((response) => console.log(response))
+        .then((response) => response.json())
         .then((data) => {
-            console.log(data);
+            return data;
         })
         .catch((error) => console.log(error));
 }
+
+// export const getUserInfo = () => {
+//
+//     axios.get('https://ce-bot.com/api/v2/home/start_web_app/463697926', {
+//         Authorization: 'Token af9bbdda4b98828b15e3a37207eed08f02dbd33a',
+//     })
+//         .then(response => console.log(response.data))
+//         .catch(error => console.log('error', error));
+// };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,22 @@
 import styles from './Home.module.scss'
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Balance from "./Balance/Balance";
 import CardsList from "./Cards/CardsList";
 import Links from "./Links/Links";
 import Accordions from "./Accordions/Accordions";
 import {useTelegram} from "../../hooks/useTelegram";
+import {getUserInfo} from "../../api/api";
 
 const Home = () => {
+    const [data, setData] = useState('');
     const {user} = useTelegram();
-    if (user) {
-        getUserInfo(user.id).then(response => console.log(response))
-    }
+    // if (user) {
+    //     getUserInfo(user.id).then(response => console.log(response))
+    // }
+    useEffect(() => {
+        getUserInfo().then(r => setData.r);
+    }, [])
+
 
     return (
         <div>
