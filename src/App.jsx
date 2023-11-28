7ab12728ad2ee0bd3000bc1429b8aea59ae49721
YuriiDzoma <devastator321678>
2023-11-28 13:30:26 +0200

    update 7.87

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     // }, [])
 
     useEffect(() => {
-        getUserInfo(user? user : 463697926).then((response) => {
+        getUserInfo(user? user.id : 463697926).then((response) => {
             console.log(response)
             dispatch(setBalance(response.user_balance));
             dispatch(setTokenName(response.token_name));
