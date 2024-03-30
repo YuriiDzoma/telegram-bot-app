f0505284374d6944dffeabd08e63350d585ca77e
YuriiDzoma <devastator321678>
2024-03-30 10:43:55 +0200

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,16 +5,26 @@ import CardsList from "./Cards/CardsList";
 import Links from "./Links/Links";
 import Accordions from "./Accordions/Accordions";
 import {useTelegram} from "../../hooks/useTelegram";
+import {Html5QrcodeScanner} from 'html5-qrcode';
+import Html5QrcodePlugin from "./Html5QrcodePlugin";
 
 const Home = () => {
-    const [data, setData] = useState('');
     const {user} = useTelegram();
 
+    const onNewScanResult = (decodedText, decodedResult) => {
+        console.log(decodedText);
+        console.log(decodedResult);
+    };
 
     return (
         <div>
             <Balance />
-            <p>test</p>
+            <Html5QrcodePlugin
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
             <div className={styles.content}>
                 <CardsList />
                 <Links />
